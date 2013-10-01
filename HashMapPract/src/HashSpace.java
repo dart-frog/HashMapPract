a2fb37defcbabbc7cd49efe2d20562eb785b6c48
ros_npswedberg <ros_npswedberg@WROSRM31917.seattleschools.org>
2013-10-01 09:53:36 -0700

    Change in assignment new object called key value pairs

--- HashMapPract/src/HashSpace.java
+++ HashMapPract/src/HashSpace.java
@@ -1,12 +1,22 @@
+import java.util.ArrayList;
+
 
 public class HashSpace {
 	public HashSpace(int size){
-		
+		ArrayList<ArrayList<KeyValuePairs>> hash = new ArrayList(); 
+		for(int i = 0; i < size; i++){
+			hash.add(ArrayList<KeyValuePairs> i);
+		}
 	}
-	public static void add(String input){
+	public static void add(String key, String value){
+		KeyValuePairs x = new KeyValuePairs(key,value);
+		int bin = key.length() % 10;
 		
 	}
 	public static void getVal(int ind){
+		
+	}
+	public static void contains(String input){
 		
 	}
 	
---
+++ HashMapPract/src/KeyValuePairs.java
@@ -0,0 +1,15 @@
+
+public class KeyValuePairs {
+	static String key;
+	static String value;
+	public KeyValuePairs(String myKey, String myValue){
+		key = myKey;
+		value = myValue;
+	}
+	public static String getValue(){
+		return value;
+	}
+	public static String getKey(){
+		return key; 
+	}
+}
