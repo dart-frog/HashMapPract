70539d73350d15173a17e03ae7bee525a58d9ace
Steve <Steve@Steve-DellPC>
2013-10-06 16:34:55 -0700

    having problems(null pointer exception);

--- HashMapPract/src/MyHashMap.java
+++ HashMapPract/src/MyHashMap.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 public class MyHashMap {
 	List<KeyValuePairs>[] myHashMap;
+	
 	public MyHashMap(int numberOfBuckets){
-
-		
 		myHashMap = new ArrayList[numberOfBuckets];
-	}
+		for (int i = 0; i <= numberOfBuckets; i++){
+		}
+	} 
+	
+	
 	public void set(String key, String value){
-		int bin = (key.length()*key.charAt(0)) % myHashMap.length;
+		int bin = (key.length()*key.charAt(0)) % (myHashMap.length - 1);
 		KeyValuePairs x = new KeyValuePairs(key,value);
 		myHashMap[bin].add(x);
 	}
@@ -20,7 +23,7 @@ public class MyHashMap {
 		int i = 0;
 		while(i < l.size()){
 			if (l.get(i).getKey().equals(key)){
-				return key;
+				return l.get(i).getValue();
 			}
 			i++;
 		}
