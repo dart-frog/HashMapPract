6f8c1cc079465f67ff098553532d83f18159d588
Steve <Steve@Steve-DellPC>
2013-10-11 14:41:24 -0700

    finished get keys

--- HashMapPract/src/MyHashMap.java
+++ HashMapPract/src/MyHashMap.java
@@ -11,22 +11,24 @@ public class MyHashMap {
 		for(int i = 0; i < numberOfBuckets; i++){
 			myHashMap[i] = new ArrayList<KeyValuePairs>();
 		}
-		/*
-		myHashMap = new ArrayList<List<KeyValuePairs>>();
-		for (int i = 0; i <= numberOfBuckets; i++){
-			myHashMap.add(new ArrayList<KeyValuePairs> (i));
-		}
-		*/
 		
 	} 
-	
-	
+	/* *
+	 * Adds a key value pair to the hashmap
+	 * @param key sets the key of the KeyValuePair
+	 * @param value sets the value of the KeyValuePair
+	*/
 	public void set(String key, String value){
 		int bin = (key.length()*key.charAt(0)) % (myHashMap.length - 1);
 		KeyValuePairs x = new KeyValuePairs(key,value);
 		myHashMap[bin].add(x);
 		x.getValue();
 	}
+	/**
+	 * Returns the value of a given key
+	 * @param key is the value you are looking for 
+	 * @return the value connected with the key or null if it is not found
+	 */
 	public String get(String key){
 		int bin = (key.length()*key.charAt(0)) % 9;
 		List<KeyValuePairs> l = myHashMap[bin];
@@ -38,10 +40,20 @@ public class MyHashMap {
 			}
 			i++;
 		}
-		return "no";
-	}
-	public List<String> getKeys(){
 		return null;
+	}
+	
+	public List<String> getKeys(){
+		ArrayList<String> xList = new ArrayList<String>();
+		for (int i = 0; i < myHashMap.length; i++){
+			for (int y = 0; y < myHashMap[i].size();y++){
+				String myKey = myHashMap[i].get(y).getKey();
+				xList.add(myKey);
+			}
+		
+		}
+		return xList;
+		
 	}
 	public boolean exists(String Key){
 		return false;
--- HashMapPract/src/MyHashMapTest.java
+++ HashMapPract/src/MyHashMapTest.java
@@ -1,19 +1,33 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 
 public class MyHashMapTest {
 
 	@Test
-	public void test() {
+	public void getTest() {
 		MyHashMap t = new MyHashMap(10);
 		t.set("berry", "black");
 		t.set("car", "bugatti");
-		assertEquals("verify again that it can return a key", "bugatti",t.get("car"));
-		assertEquals("verify that it can return a key", "black", t.get("berry"));
-		
-		
+		t.set("can", "tuna");
+		assertEquals("verify that it can return a key", "bugatti",t.get("car"));
+		assertEquals("verify that it can choose between keys", "black", t.get("berry"));
+		assertEquals("verify that it cam choose between two keys from the same bucket", "tuna", t.get("can"));
+	}
+	@Test
+	public void getKeysTest() {
+		MyHashMap t = new MyHashMap(10);
+		t.set("berry", "black");
+		t.set("car", "bugatti");
+		t.set("can", "tuna");
+		ArrayList<String> myT = new ArrayList<String>();
+		myT.add("car");
+		myT.add("can");
+		myT.add("berry");
+		assertEquals("verify that it can return a list of strings", myT,t.getKeys());
 	}
 
 }
