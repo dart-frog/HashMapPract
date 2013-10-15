8eba3298894dc30364a1e4206957282d6ad88067
ros_npswedberg <ros_npswedberg@WROSRM31919.seattleschools.org>
2013-10-15 09:52:51 -0700

    Have colomns but need to monkey around with it to make it look nice

--- HashMapPract/src/Screen.java
+++ HashMapPract/src/Screen.java
@@ -30,18 +30,20 @@ public class Screen {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel p = new JPanel();
 		
-		for (int i = 1; i <= t.size(); i++){
-			JLabel menuLabel = new JLabel("bucket " + i);
-			menuLabel.setBackground(new Color(255, 10, 159));        
-			menuLabel.setPreferredSize(new Dimension(120, 60));
-			p.add(menuLabel, GridLayout..HORIZONTAL);
-		}
+
 		
 		
 		ArrayList<String> keys = (ArrayList<String>) t.getKeys();
 		int formalSize = 0;
-		
+		Box hBox = Box.createHorizontalBox();
 		for (int i = 0; i < t.size(); i++){
+			JLabel menuLabel = new JLabel("bucket " + (i + 1));
+			menuLabel.setBackground(new Color(255, 10, 159));        
+			menuLabel.setPreferredSize(new Dimension(120, 60));
+			
+			Box vBox = Box.createVerticalBox();
+			vBox.add(menuLabel);
+			
 			for (int j = 0; j < t.bucketSize(i); j++){
 				String myKey = keys.get(formalSize);
 				String myVal = t.get(myKey);
@@ -54,10 +56,11 @@ public class Screen {
 				yellowLabel.setPreferredSize(new Dimension(120, 60));
 			
 				formalSize++;
-				p.add(yellowLabel, BorderLayout.NORTH);
+				vBox.add(yellowLabel);
 			}
+			p.add(BorderLayout.NORTH, vBox);
 		}
-		
+		//p.add(BorderLayout.EAST,hBox);
 		
 		
 		
